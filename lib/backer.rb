867e2e0ffa8bcfe0350d33e58c4c8c83e81493de
Eva Navarro <emanao@me.com>
2019-11-14 17:59:34 +0000

    Maintain a single source of truth

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,11 +1,24 @@
+require 'pry'
 class Backer
-  attr_accessor :name, :backed_projects
+  attr_accessor :name, :project
+  @@all = []
+  def self.all
+    @@all.dup.freeze
+  end
+  def save
+    @@all<<self
+  end
   def initialize(name)
     @name=name
-    @backed_projects=[]
+    save
   end
-  def back_project(project)
-    backed_projects<<project
-    project.add_backer(self) unless project.backers.include?(self)
+  def back_project(project)associationtypemismatch
+    raise AssociationTypeMismatchError,"#{project.class} received, Project expected."
+    project.backer = self
+    #backed_projects<<project
+    #project.add_backer(self) unless project.backers.include?(self)
+  end
+  def projects
+    Project.all.select{|project| project.backer==self}
   end
 end
--- lib/project.rb
+++ lib/project.rb
@@ -1,11 +1,23 @@
 class Project
-  attr_accessor :title, :backers
+  attr_accessor :title, :backer
+  @@all = []
+  def self.all
+    @@all.dup.freeze
+  end
+  def save
+    @@all<<self
+  end
   def initialize(title)
     @title=title
-    @backers=[]
+    save
   end
   def add_backer(backer)
-    backers<<backer
-    backer.back_project(self) unless backer.backed_projects.include?(self)
+    raise AssociationTypeMismatchError,"#{backer.class} received, Backer expected."
+    backer.project=self
+    #backers<<backer
+    #backer.back_project(self) unless backer.backed_projects.include?(self)
+  end
+  def backers
+    Backer.all.select{|backer| backer.project==self}
   end
 end
