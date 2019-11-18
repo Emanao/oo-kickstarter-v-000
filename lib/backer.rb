45240954e5a0dd6e0996d7275d3bd1c2cb3df233
Eva Navarro <emanao@me.com>
2019-11-18 21:48:11 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,11 +1,33 @@
 class Backer
-  attr_accessor :name, :backed_projects
+  attr_accessor :name, :project
+  @@all = []
+  #@@projects = []
+  def self.all
+    @@all.dup.freeze
+  end
+
+  def save
+    @@all<<self
+  end
+
   def initialize(name)
     @name=name
-    @backed_projects=[]
+    save
   end
-  def back_project(project)
-    backed_projects<<project
-    project.add_backer(self) unless project.backers.include?(self)
+  #
+  # def back_project(project)
+  #   raise AssociationTypeMismatchError,"#{project.class} received, Project expected." if !project.is_a?(Project)
+  #   project.backer = self
+  #   #backed_projects<<project
+  #   #project.add_backer(self) unless project.backers.include?(self)
+  # end
+
+  def add_project_by_name(name)
+    project = Project.new(name)
+    project.backer = self
+  end
+
+  def projects
+    Project.all.select{|project| project.backer.name==self.name}
   end
 end
--- lib/project.rb
+++ lib/project.rb
@@ -1,11 +1,33 @@
 class Project
-  attr_accessor :title, :backers
-  def initialize(title)
-    @title=title
-    @backers=[]
+  attr_accessor :name, :backer
+  @@all = []
+
+  def self.all
+    @@all.dup.freeze
   end
-  def add_backer(backer)
-    backers<<backer
-    backer.back_project(self) unless backer.backed_projects.include?(self)
+
+  def save
+    @@all<<self
+  end
+
+  def initialize(name)
+    @name=name
+    save
+  end
+  #
+  # def add_backer(backer)
+  #   raise AssociationTypeMismatchError,"#{backer.class} received, Backer expected." if !backer.is_a?(Backer)
+  #     backer.project=self
+  #   #backers<<backer
+  #   #backer.back_project(self) unless backer.backed_projects.include?(self)
+  # end
+
+  def add_backer_by_name(name)
+    backer = Backer.new(name)
+    backer.project = self
+  end
+
+  def backers
+    Backer.all.select{|backer| backer.project.name==self.name}
   end
 end
