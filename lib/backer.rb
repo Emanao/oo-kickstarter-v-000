3962e7279a370014fa1a924785e56c32910a5084
Eva Navarro <emanao@me.com>
2019-11-18 17:25:27 +0000

    Build Has-many relationship through strings

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,23 +1,33 @@
 class Backer
   attr_accessor :name, :project
   @@all = []
+  #@@projects = []
   def self.all
     @@all.dup.freeze
   end
+
   def save
     @@all<<self
   end
+
   def initialize(name)
     @name=name
     save
   end
-  def back_project(project)associationtypemismatch
-    raise AssociationTypeMismatchError,"#{project.class} received, Project expected." if !project.is_a?(Project)
+  #
+  # def back_project(project)
+  #   raise AssociationTypeMismatchError,"#{project.class} received, Project expected." if !project.is_a?(Project)
+  #   project.backer = self
+  #   #backed_projects<<project
+  #   #project.add_backer(self) unless project.backers.include?(self)
+  # end
+
+  def add_project_by_name(name)
+    project = Project.new(name)
     project.backer = self
-    #backed_projects<<project
-    #project.add_backer(self) unless project.backers.include?(self)
   end
+
   def projects
-    Project.all.select{|project| project.backer==self}
+    Project.all.select{|project| project.backer.name==self.name}
   end
 end
--- lib/project.rb
+++ lib/project.rb
@@ -1,23 +1,33 @@
 class Project
-  attr_accessor :title, :backer
+  attr_accessor :name, :backer
   @@all = []
+
   def self.all
     @@all.dup.freeze
   end
+
   def save
     @@all<<self
   end
-  def initialize(title)
-    @title=title
+
+  def initialize(name)
+    @name=name
     save
   end
-  def add_backer(backer)
-    raise AssociationTypeMismatchError,"#{backer.class} received, Backer expected." if !backer.is_a?(Backer)
-    backer.project=self
-    #backers<<backer
-    #backer.back_project(self) unless backer.backed_projects.include?(self)
+  #
+  # def add_backer(backer)
+  #   raise AssociationTypeMismatchError,"#{backer.class} received, Backer expected." if !backer.is_a?(Backer)
+  #     backer.project=self
+  #   #backers<<backer
+  #   #backer.back_project(self) unless backer.backed_projects.include?(self)
+  # end
+
+  def add_backer_by_name(name)
+    backer = Backer.new(name)
+    backer.project = self
   end
+
   def backers
-    Backer.all.select{|backer| backer.project==self}
+    Backer.all.select{|backer| backer.project.name==self.name}
   end
 end
