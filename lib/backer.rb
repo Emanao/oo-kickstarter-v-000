3833e6675f3ecae7d77503e6fa1722632af822d2
Eva Navarro <emanao@me.com>
2019-11-18 21:49:34 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,33 +1,11 @@
 class Backer
-  attr_accessor :name, :project
-  @@all = []
-  #@@projects = []
-  def self.all
-    @@all.dup.freeze
-  end
-
-  def save
-    @@all<<self
-  end
-
+  attr_accessor :name, :backed_projects
   def initialize(name)
     @name=name
-    save
+    @backed_projects=[]
   end
-  #
-  # def back_project(project)
-  #   raise AssociationTypeMismatchError,"#{project.class} received, Project expected." if !project.is_a?(Project)
-  #   project.backer = self
-  #   #backed_projects<<project
-  #   #project.add_backer(self) unless project.backers.include?(self)
-  # end
-
-  def add_project_by_name(name)
-    project = Project.new(name)
-    project.backer = self
-  end
-
-  def projects
-    Project.all.select{|project| project.backer.name==self.name}
+  def back_project(project)
+    backed_projects<<project
+    project.add_backer(self) unless project.backers.include?(self)
   end
 end
